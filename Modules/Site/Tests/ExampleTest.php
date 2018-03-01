394288b475aaad19770bda34bab335009072f7fd
Alison Valente <alisonvm@gmail.com>
2018-03-01 16:08:41 -0300

    test map modules

---
+++ Modules/Site/Tests/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Site\Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SiteTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
