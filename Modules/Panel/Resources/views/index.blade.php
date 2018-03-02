2e438ad0fd04a504428a956d76b1e761728147d8
Alison Valente <alisonvm@gmail.com>
2018-03-02 15:30:48 -0300

    squares

--- Modules/Panel/Resources/views/index.blade.php
+++ Modules/Panel/Resources/views/index.blade.php
@@ -2,6 +2,19 @@
 
 @section('head-includes')
     @include('core::layouts.angular.includes')
+    <style>
+        .square {
+            margin: 10px;
+            height: 60px;
+            width: 60px;
+            border-radius: 10px;
+            border: 3px solid white;
+        }
+        .color1 {background-color: #FFBD33;}
+        .color2 {background-color: #33FFBD;}
+        .color3 {background-color: #FF5733;}
+        .color4 {background-color: #DBFF33;}
+    </style>
 @stop
 
 @section('content')
@@ -25,6 +38,17 @@
                         <div class="form-group">
                             <label>E-mail:</label>
                             <input type="text" class="form-control" ng-model="site.description">
+                        </div>
+                        <div class="form-group">
+                            <label>Cor de fundo:</label>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-1 square color1"></div>
+                                    <div class="col-md-1 square color2"></div>
+                                    <div class="col-md-1 square color3"></div>
+                                    <div class="col-md-1 square color4"></div>
+                                </div>
+                            </div>
                         </div>
                         <div class="form-group text-center">
                             <button type="button" class="btn btn-primary" ng-click="update()">Salvar</button>
